6e45607d9b4543288f21e35fa96efe7482512e6d
Linh Nguyen <linh0404@hotmail.com>
2019-07-07 23:12:08 +1000

    psedocode the javascript section

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// PSEUDOCODE
+
+// 1. randomly generate a number between 19-120 and display on page
+// 2. randomly generate 4 numbers between 1-12 adn assign to the four buttons
+// 3. create a calculator so that each time each button is pressed, it adds to the total score
+// 4. if else statement:
+// if score > target, you lose
+// if score < target, continue
+// if score = target, you win
